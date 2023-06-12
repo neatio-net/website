f7d65cc11d293e67d218a89a1488d452c19975e9
MrTizu <99123684+MrTizu@users.noreply.github.com>
2023-06-12 22:05:28 +0300

    Create AppCopyright.vue

---
+++ components/AppCopyright.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-text font="sans" size="1rem">
+      <a href="/terms/"
+              >
+    <v-text size="1rem">Terms</v-text></a
+            >
+      © {{ currentYear }} Neatio.
+    </v-text>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'AppCopyright',
+  computed: {
+    currentYear() {
+      return new Date().getFullYear()
+    },
+  },
+}
+</script>
+
+<style lang="css" scoped>
+div {
+  padding-bottom: 2.5rem;
+  text-align: center;
+}
+</style>
