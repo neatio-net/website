403b3a33b957ab3d70d084820eace34d77b463d4
MrTizu <99123684+MrTizu@users.noreply.github.com>
2023-06-12 22:05:46 +0300

    Create AppHeader.vue

---
+++ components/AppHeader.vue
@@ -0,0 +1,48 @@
+<template>
+  <header class="app-header">
+    <v-header
+      logo="images/nio3-logo.png"
+   
+      :menu-items="menuItems"
+    />
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'AppHeader',
+  data() {
+    return {
+      // headerCta: {
+      //   label: 'Start Building',
+      //   link: process.env.NEATIO_DASHBOARD_URL,
+      // },
+      menuItems: [
+      {
+          name: 'Team',
+          link: '/about/',
+        },
+        {
+          name: 'Whitepaper',
+          link: '/whitepaper/',
+          // external: true,
+        },
+        {
+          name: 'Roadmap',
+          link: '/tech/',
+        },
+
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped>
+.app-header {
+  position: sticky;
+  top: 0;
+  z-index: 100;
+  background-color: #0f0f0f;
+}
+</style>
