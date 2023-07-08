7ff0bbd93a29bd1e95c6e8c6d4ef8dac986b8757
MrTizu <99123684+MrTizu@users.noreply.github.com>
2023-07-08 22:14:39 +0300

    Create index.vue

---
+++ pages/index.vue
@@ -0,0 +1,26 @@
+<template>
+  <main>
+    <HomeHeroSection />
+    <HomeOurServices />
+    <HomeOurMission />
+    <HomeRoadmap />
+  </main>
+</template>
+
+<script>
+export default {
+  layout: 'page',
+}
+</script>
+
+<style lang="css" scoped>
+@import url('../components/lib/media-query-helper.css');
+
+main {
+  overflow-x: hidden;
+}
+
+main > * {
+  margin-top: 6rem;
+}
+</style>
